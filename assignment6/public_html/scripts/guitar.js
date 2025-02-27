ce92eaecd969dd4c6a7e4bde7da6d60e9b21a4bf
jay <42817289+jae1Heo@users.noreply.github.com>
2025-02-26 19:14:02 -0800

    Add files via upload

---
+++ assignment6/public_html/scripts/guitar.js
@@ -0,0 +1,37 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+"use strict";
+
+
+
+
+$(document).ready(() => { 
+    let index = 1;
+    // initializing slider
+   $(".bxslider").bxSlider({
+       randomStart: true, // starts with random image
+       captions: true, // make caption to visible 
+       pager: false, // removing pager icon
+       slideWidth: 200, // width of slider image
+       pause: 3000, // shows 3 seconds and move to the next image
+       auto: true, // moves slide automatically
+       controls: false, // remove control buttons on both sides
+       onSlideAfter: () => { // callback api, called after swipped. 
+           if(index >= total) { // if default number is greater than total,
+               index = 1; // reset the index
+           }
+           else {
+               index++; // otherwise, increase index
+           }
+           $(".number").text(index + "/" + total); // update text
+       }
+   });
+   
+   let total = 6; // number of total images in the slide
+   $(".number").text(index + "/" + total); // default text
+   
+});
